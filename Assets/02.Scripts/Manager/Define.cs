4a7bf17cce1ae0d039ddae88c94878536d3fa1c5
Hone Won Gi <keyone957@naver.com>
2024-07-06 21:11:39 +0900

    기초세팅 및 사운드 매니저 추가
    기초세팅

---
+++ Assets/02.Scripts/Manager/Define.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Define
+{
+    public const string bgmRoot = "BGM";
+    public const string soundRoot = "Sound";
+
+}
---
+++ Assets/02.Scripts/Manager/SoundManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _bgmSource = null;
+    [SerializeField] private AudioSource _soundSource = null;
+    public static SoundManager _instance { get; private set; }
+
+    private Dictionary<string, AudioClip> _loadedClip = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+    }
+    
+    private AudioClip LoadAudioClip(string fullPath)
+    {
+        AudioClip clip = null;
+        if (_loadedClip.TryGetValue(fullPath, out clip))
+            return clip;
+
+        clip = Resources.Load<AudioClip>(fullPath);
+        if (clip == null)
+        {
+            Debug.LogError($"[SoundManager.LoadAudioClip.InvalidPath]{fullPath}");
+            return null;
+        }
+
+        _loadedClip.Add(fullPath, clip);
+        return clip;
+    }
+
+    #region BGM
+    
+
+    private static string GetBGMFullPath(string path) => Define.bgmRoot + "/" + path;
+    public void LoadBGM(string path) => LoadAudioClip(GetBGMFullPath(path));
+
+    public void PlayBGM(string path)
+    {
+        AudioClip clip = LoadAudioClip(GetBGMFullPath(path));
+        if (clip == null)
+            return;
+
+        _bgmSource.clip = clip;
+        _bgmSource.Play();
+    }
+
+    public void StopBGM() => _bgmSource.Stop();
+
+    #endregion
+
+    #region SoundEffect
+
+    private static string GetSoundFullPath(string path) => Define.soundRoot + "/" + path;
+
+    public void LoadSound(string path) => LoadAudioClip(GetSoundFullPath(path));
+
+    public void PlaySound(string path)
+    {
+        AudioClip clip = LoadAudioClip(GetSoundFullPath(path));
+        if (clip == null)
+            return;
+        _soundSource.PlayOneShot(clip);
+    }
+
+    public void ClearLoadedAudioClip() => _loadedClip.Clear();
+    public void StopSound() => _soundSource.Stop();
+
+    #endregion
+}
